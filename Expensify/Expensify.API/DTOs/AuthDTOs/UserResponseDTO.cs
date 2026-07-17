6e39e319b02b9f2daca0459affe7d518af83c03e
DLew <96327749+DLew2024@users.noreply.github.com>
2026-07-17 00:26:48 -0500

    Add Projection

--- Expensify/Expensify.API/DTOs/AuthDTOs/UserResponseDTO.cs
+++ Expensify/Expensify.API/DTOs/AuthDTOs/UserResponseDTO.cs
@@ -6,10 +6,10 @@ namespace Expensify.API.DTOs.AuthDTOs;
 
 public class UserResponseDTO
 {
-    public Guid Id { get; init; }
-    public string FullName { get; init; } = string.Empty;
-    public string Email { get; init; } = string.Empty;
-    public string ProfileImageUrl { get; init; } = string.Empty;
+    public required Guid Id { get; init; }
+    public required string FullName { get; init; } 
+    public required string Email { get; init; } 
+    public required string ProfileImageUrl { get; init; }
 
     public static UserResponseDTO FromUser(User user)
     {
@@ -25,6 +25,7 @@ public class UserResponseDTO
     public static Expression<Func<User, UserResponseDTO>> Projection =>
         user => new UserResponseDTO
         {
+            Id = user.Id,
             FullName = user.FullName,
             Email = user.Email,
             ProfileImageUrl = user.ProfileImageUrl,
