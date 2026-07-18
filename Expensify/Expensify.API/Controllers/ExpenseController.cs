f070d8d90fc2d524e91cb1093247613481d96197
DLew <96327749+DLew2024@users.noreply.github.com>
2026-07-18 15:25:22 -0500

    Add Expense Controller and DTOs

--- Expensify/Expensify.API/Controllers/ExpenseController.cs
+++ Expensify/Expensify.API/Controllers/ExpenseController.cs
@@ -1,4 +1,10 @@
-﻿using Expensify.API.ServiceClasses.Interfaces;
+﻿using System.Security.Claims;
+using Expensify.API.DTOs.ExpenseDTOs;
+using Expensify.API.DTOs.IncomeDTOs;
+using Expensify.API.ServiceClasses.Interfaces;
+using Expensify.API.Utility.GlobalExceptionHandling.CustomExceptions;
+using LanguageExt.ClassInstances;
+using LanguageExt.Pipes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Expensify.API.Controllers
@@ -12,16 +18,82 @@ namespace Expensify.API.Controllers
             _service = service;
         }
 
-        [HttpPost(Name = "AddExpense")]
-        public void AddExpense() { }
+        [HttpPost("add", Name = "AddExpense")]
+        public async Task<ActionResult<ExpenseTransactionResponseDTO>> AddExpense(
+            AddExpenseTransactionDTO request,
+            CancellationToken cancellationToken
+        )
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            var result = await _service.ExpenseService.AddExpense(
+                userId,
+                request,
+                cancellationToken
+            );
 
-        [HttpGet(Name = "GetAllExpenses")]
-        public void GetAllExpenses() { }
+            return result.Match<ActionResult<ExpenseTransactionResponseDTO>>(
+                success => StatusCode(StatusCodes.Status201Created, success),
+                error =>
+                    error switch
+                    {
+                        EntityNotFoundException => BadRequest(error.Message),
+                        _ => StatusCode(StatusCodes.Status500InternalServerError, error.Message),
+                    }
+            );
+        }
 
-        [HttpGet(Name = "DownloadExpenseExcel")]
-        public void DownloadExpenseExcel() { }
+        [HttpGet("downloadExcel", Name = "DownloadExpenseExcel")]
+        public async Task<ActionResult<bool>> DownloadExpenseExcel(
+            DownloadExpenseExcelDTO request,
+            CancellationToken cancellationToken
+        )
+        {
+            var result = await _service.ExpenseService.DownloadExpenseExcel(
+                request,
+                cancellationToken
+            );
+            return result.Match<ActionResult<bool>>(
+                success => StatusCode(StatusCodes.Status200OK, success),
+                error =>
+                    error switch
+                    {
+                        _ => StatusCode(StatusCodes.Status500InternalServerError, error.Message),
+                    }
+            );
+        }
 
-        [HttpDelete(":{id}")]
-        public void DeleteExpense() { }
+        [HttpGet("getAll", Name = "GetAllExpenses")]
+        public async Task<ActionResult<bool>> GetAllExpenses(
+            GetExpenseIncomeDTO request,
+            CancellationToken cancellationToken
+        )
+        {
+            var result = await _service.ExpenseService.GetAllExpense(request, cancellationToken);
+            return result.Match<ActionResult<bool>>(
+                success => StatusCode(StatusCodes.Status200OK, success),
+                error =>
+                    error switch
+                    {
+                        _ => StatusCode(StatusCodes.Status500InternalServerError, error.Message),
+                    }
+            );
+        }
+
+        [HttpDelete(":{id}", Name = "Delete Expense")]
+        public async Task<ActionResult<bool>> DeleteExpense(
+            Guid id,
+            CancellationToken cancellationToken
+        )
+        {
+            var result = await _service.ExpenseService.DeleteExpense(id, cancellationToken);
+            return result.Match<ActionResult<bool>>(
+                success => StatusCode(StatusCodes.Status204NoContent, id),
+                error =>
+                    error switch
+                    {
+                        _ => StatusCode(StatusCodes.Status500InternalServerError, error.Message),
+                    }
+            );
+        }
     }
 }
--- Expensify/Expensify.API/DTOs/DashboardDTOs/DashboardDataResponseDTO.cs
+++ Expensify/Expensify.API/DTOs/DashboardDTOs/DashboardDataResponseDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq.Expressions;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
 using Expensify.DataAccessLayer.Entities.Models;
 using Expensify.DataAccessLayer.Enums;
 
@@ -6,8 +7,13 @@ namespace Expensify.API.DTOs.DashboardDTOs;
 
 public class DashboardDataResponseDTO
 {
+    [Required]
     public decimal TotalBalance { get; set; }
+
+    [Required]
     public decimal TotalIncome { get; set; }
+
+    [Required]
     public decimal TotalExpenses { get; set; }
     public TransactionPeriodSummaryDTO? Last30DaysOfExpenses { get; set; }
     public TransactionPeriodSummaryDTO? Last60DaysOfExpenses { get; set; }
@@ -24,20 +30,25 @@ public class TransactionPeriodSummaryDTO
 
 public class TransactionDTO
 {
+    [Required]
     public Guid Id { get; set; }
+
+    [Required]
     public decimal Amount { get; set; }
+
+    [Required]
+    public TransactionType Type { get; set; }
     public long TransactionDate { get; set; }
     public string Merchant { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Notes { get; set; } = string.Empty;
     public required PaymentMethodDTO PaymentMethod { get; set; }
-    public TransactionType Type { get; set; }
+    public required CategoryDTO Category { get; set; }
     public TransactionPostedStatus Status { get; set; }
-    public Guid CategoryId { get; set; }
-    public string CategoryName { get; set; } = string.Empty;
     public bool IsRecurring { get; set; }
     public Guid RecurringTransactionId { get; set; }
     public List<string> Tags { get; set; } = [];
+    public string Icon { get; set; } = string.Empty;
 
     public static readonly Expression<Func<Transaction, TransactionDTO>> Projection =
         transaction => new TransactionDTO
@@ -58,14 +69,28 @@ public class TransactionDTO
                     : new PaymentMethodDTO { Id = Guid.Empty, Name = string.Empty },
             Type = transaction.Type,
             Status = transaction.Status,
-            CategoryId = transaction.CategoryId ?? Guid.Empty,
-            CategoryName = transaction.Category != null ? transaction.Category.Name : string.Empty,
+            Category =
+                transaction.Category != null
+                    ? new CategoryDTO
+                    {
+                        Id = transaction.Category.Id,
+                        Name = transaction.Category.Name,
+                    }
+                    : new CategoryDTO { Id = Guid.Empty, Name = string.Empty },
+
             IsRecurring = transaction.IsRecurring,
             Tags = transaction.Tags,
         };
 }
 
 public class PaymentMethodDTO
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+}
+
+public class CategoryDTO
 {
     public Guid Id { get; set; }
 
